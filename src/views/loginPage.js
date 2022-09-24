2b865c67e250c4980feabbeca4b657358d9dded0
jeongdonggyeom <topcome928@gmail.com>
2022-09-24 15:58:46 +0900

    FEAT :: login ,signup

--- src/views/loginPage.js
+++ src/views/loginPage.js
@@ -1,16 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 import Header from "./header";
 import '../styles/styles.css';
+import { instance } from "../instance";
 
-export default function loginPage(){
+export default function LoginPage()
+{
+    const [login, setLogin] = useState({
+        username: "",
+        password: ""
+    });
+
+    const change = (e) => {
+        const { name, value } = e.target;
+        const newInput = {
+            ...login,
+            [name]: value
+        };
+        setLogin(newInput);
+    }
+
+    const postLogin = async () => {
+        try{
+            await instance.post('/login', login)
+            alert('성공');
+        }catch(error){
+            alert('실패');
+            console.log(error);
+        }
+    }
+
     return(
         <loginPage>
             <Header />
             <Login>로그인</Login>
-            <input type="text" className ="idbox_l" placeholder="아이디"/><br />
-            <input type="password" className="pwbox_l" placeholder="비밀번호" /><br />
-            <button className="logbutton">로그인</button>
+            <input type="text" className ="idbox_l" name="username" value={login.username} onChange={(e)=>change(e)} placeholder="아이디"/><br />
+            <input type="password" className="pwbox_l" name="password" value={login.password} onChange={(e)=>change(e)} placeholder="비밀번호" /><br />
+            <button className="logbutton" onClick={postLogin}>로그인</button>
         </loginPage>
     )
 }
--- src/views/mainPage.js
+++ src/views/mainPage.js
@@ -2,7 +2,7 @@ import React from "react";
 import Header from "./header"
 import '../styles/styles.css';
 
-export default function mainPage(){
+export default function MainPage(){
     return(
         <div>
             <Header />
